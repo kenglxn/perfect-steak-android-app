5b24b9eb83be6023cba47046dc7cd504342fcf8f
Ken Gullaksen <ken@glxn.net>
2012-04-08 19:35:43 +0200

    disabled home button, added quit dialog and added launcher icons

--- src/main/java/net/glxn/android/Main.java
+++ src/main/java/net/glxn/android/Main.java
@@ -3,8 +3,10 @@ package net.glxn.android;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
@@ -14,6 +16,7 @@ import android.os.CountDownTimer;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -31,48 +34,57 @@ public class Main extends Activity {
     private TextView timerView;
     private int iteration;
     private MediaPlayer mediaPlayer;
+    private CountDownTimer countDownTimer;
+    private Dialog dialog;
+    private TextView textView;
+    private Button startCookingBtn;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Log.v(TAG, "Activity State: onCreate()");
+        Log.v(TAG, "onCreate()");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
         this.timerView = (TextView) this.findViewById(R.id.timer);
 
-        final Button startCookingBtn = (Button) findViewById(R.id.startCookingBtn);
+        startCookingBtn = (Button) findViewById(R.id.startCookingBtn);
         startCookingBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d(TAG, "startCookingBtn clicked");
+                Log.d(TAG, "startCookingBtn.onClick()");
 
-                final Dialog dialog = new Dialog(Main.this);
+                dialog = new Dialog(Main.this);
                 dialog.setContentView(R.layout.custom_dialog);
-                final TextView textView = (TextView) dialog.findViewById(R.id.text);
+                textView = (TextView) dialog.findViewById(R.id.text);
                 textView.setText("Place 'em on the grill");
                 iteration = 0;
                 ImageView imageView = (ImageView) dialog.findViewById(R.id.image);
 
                 dialog.findViewById(R.id.okBtn).setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
-                        Log.d(TAG, "okBtn clicked");
+                        Log.d(TAG, "okBtn.onClick()");
+
                         startCookingBtn.setVisibility(View.GONE);
                         iteration++;
 
-                        new CountDownTimer(iterationTimeMillis, ONE_SEC_IN_MILLIS) {
+                        countDownTimer = new CountDownTimer((long) iterationTimeMillis, ONE_SEC_IN_MILLIS) {
 
                             public void onTick(long millisUntilFinished) {
+                                Log.d(TAG, "onTick() " + millisUntilFinished);
+
                                 timerView.setText("" + millisUntilFinished / 1000);
                             }
 
                             public void onFinish() {
+                                Log.d(TAG, "onFinish()");
+
                                 try {
                                     beep();
                                 } catch (IOException ignored) {}
 
                                 timerView.setText("0");
-                                if(iteration < 4) {
+                                if (iteration < 4) {
                                     textView.setText("Flip 'em " + (iteration == 1 ? "" : "again"));
                                     dialog.show();
                                 } else if (iteration == 4) {
@@ -83,7 +95,8 @@ public class Main extends Activity {
                                     timerView.setText("You're done. Enjoy!");
                                 }
                             }
-                        }.start();
+                        };
+                        countDownTimer.start();
 
                         dialog.dismiss();
                     }
@@ -95,7 +108,53 @@ public class Main extends Activity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        Log.d(TAG, "onBackPressed()");
+
+        new AlertDialog.Builder(this)
+            .setMessage(R.string.quit_alert_message)
+            .setTitle(R.string.rating_exit_title)
+            .setCancelable(false)
+            .setPositiveButton(android.R.string.ok,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        Main.super.onBackPressed();
+                    }
+                })
+            .setNeutralButton(android.R.string.cancel,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                    }
+                })
+            .show();
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        Log.d(TAG, "Disable Home Button by setting window type to TYPE_KEYGUARD");
+
+        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause()");
+
+        super.onPause();
+        if(countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        if(mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+    }
+
     private void beep() throws IOException {
+        Log.d(TAG, "beep()");
+
         Vibrator vibrator;
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(200);
@@ -112,24 +171,4 @@ public class Main extends Activity {
         }
     }
 
-    private void cleanUp() {
-        if(mediaPlayer != null) {
-            mediaPlayer.release();
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        cleanUp();
-    }
-
-    @Override
-    protected void onPause() {
-        cleanUp();
-    }
-
-    @Override
-    protected void onDestroy() {
-        cleanUp();
-    }
 }
